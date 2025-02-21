4d7c30b7751c6aafff5e26e4c3ab76f17e6ae984
Rita Zhang <ritazhang@Ritas-MacBook-Air.local>
2025-02-20 20:27:49 -0500

    LAZY:

---
+++ lesson2.js
@@ -0,0 +1,19 @@
+// Primitives
+
+// Numbers:
+// floats (decimal) and integers
+
+let myFloat = 1.0;
+let myInt = 1;
+
+// String:
+// sequence of characters
+let myString = "Lily is obsessed";
+
+// Boolean:
+// true or false
+
+let myBoolean = true;
+
+
+let isRitaFunny
--- test.js
+++
@@ -1,1 +0,0 @@
-console.log("lily is a coward")
